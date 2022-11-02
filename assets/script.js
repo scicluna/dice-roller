deb753e2480919a82a894b244ad6654a0706bd8f
scicluna <88948700+scicluna@users.noreply.github.com>
2022-11-01 23:34:14 -0400

    added some comments

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
+//Variables that will be able to be filled out on our webpage later
 var diceSize = 6
 var diceQuantity = 3
+//Placeholder for the results and eventual display
 var diceResults = []
 
+//Function that rolls a single dice
 function rollDice(){
     result = Math.ceil((Math.random())*diceSize)
     return result
 }
 
+//Function that rolls multiple dice and totals them
 function rollAll(){
     var total = 0
 for (i=0;i<diceQuantity;i++){
@@ -19,5 +23,6 @@ for (i=0;i<diceResults.length;i++){
 console.log(total)
 }
 
+//Run
 rollAll();
 console.log(diceResults)
