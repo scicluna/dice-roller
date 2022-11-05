6fd508c01951df606a6c827afc54a7c9357dd3f8
scicluna <88948700+scicluna@users.noreply.github.com>
2022-11-05 14:50:30 -0400

    Actually combined exploding and drop dice functions so thats its better

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ function addArray(array){
     for (let i of array){
         total += i
     }
-    console.log(total)
     return total
 }
 //This function lets me turn a string into an array of characters. It also lets me switch on the "explodingflag" tag should the string end with an !
@@ -17,59 +16,38 @@ function deString(string){
     return usingSpread
 }
 
-//This function handles dropping dice. It generates and resorts an array while slicing away the desired amount of sliced die from the bottom
-function dropOne(size,quantity,drop){
+//This function handles exploding/dropping dice
+function dropExplode(size, quantity,drop){
     let dice =[]
-    
-            for (let i=0; i<quantity; i++){
-                let die = Math.ceil(Math.random()*size)
-                dice.push(die)
-        }
-
-    console.log(dice)
-    if (drop !== undefined){
-    var diceSort = dice.sort((a,b) => b-a).slice(0,dice.length-drop)
-    console.log(dice)
-    console.log(diceSort)
-    addArray(diceSort)
-    document.getElementById("adice").value = dice;
-    document.getElementById("total").value = total;
-    }
-    else {
-    var diceSort = dice.sort((a,b) => b-a)
-    console.log(dice)
-    console.log(diceSort)
-    addArray(diceSort)
-    document.getElementById("adice").value = dice;
-    document.getElementById("total").value = total;
-    }
-}
-
-//This function handles exploding dice. I don't know why the flag works here considering that it isn't a variable - but it sure does work 
-function explodingDice (size, quantity){
-    let explodingdicearray = []
-    for (let i=0;i<quantity;i++){
-        let explodingdice = Math.ceil(Math.random()*size)
-        if (explodingdice == size){
-            i--
-            console.log('EXPLODE')
-        }
-        explodingdicearray.push(explodingdice)
-    }
-    addArray(explodingdicearray);
-    document.getElementById("adice").value = explodingdicearray;
-    document.getElementById("total").value = total;
-}
-
-//This function handles the use condiotions of either exploding dice or drop dice. I don't have a solution for using both yet.
-function allRoll(size, quantity, drop){
     if (explodingflag == "on"){
-        explodingDice(size,quantity)
+    for (let i=0;i<quantity;i++){
+        let die = Math.ceil(Math.random()*size)
+        if (die == size){
+            i--
+            console.log('EXPLOSIIIOOOON')
+        }
+        dice.push(die)
     }
+}
     else {
-        dropOne(size,quantity,drop)
+    for (let i=0; i<quantity; i++){
+        let die = Math.ceil(Math.random()*size)
+        dice.push(die)
     }
-    explodingflag = "off"
+}
+if (drop !== undefined){
+var diceSort = dice.sort((a,b) => b-a).slice(0,dice.length-drop)
+addArray(diceSort)
+document.getElementById("adice").value = dice;
+document.getElementById("total").value = total;
+}
+else {
+var diceSort = dice.sort((a,b) => b-a)
+addArray(diceSort)
+document.getElementById("adice").value = dice;
+document.getElementById("total").value = total;
+}
+explodingflag = "off"
 }
 
-explodingflag ="off"
+
